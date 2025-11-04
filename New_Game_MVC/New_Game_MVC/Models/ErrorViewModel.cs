e6eec338254a6d5eff8326fcdc3796104625b7f2
Iman Ali <iman@example.com>
2025-11-04 21:57:15 +0200

    test2

---
+++ New_Game_MVC/New_Game_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace New_Game_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
